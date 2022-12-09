6c9d04fa8bd902ca7de97cb429a9f1c8df1f9089
Max Zavaleta <mzavaletav@gmail.com>
2022-12-08 22:28:12 -0500

    feat: add dockerfile

--- test-app/Program.cs
+++ test-app/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int x=1;
+int y=4;
+int result= Calc.add(x,y);
+Console.WriteLine($"Calc add: x=>{x}+ y=>{y}= {result}");
